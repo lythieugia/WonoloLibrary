cf9114e96b6d642a540d8e2a5c35f1ecc8740acc
Tuan Anh Ly <tuananh@wonolo.com>
2020-01-07 15:42:22 +0700

    update

--- wolibrary/src/main/java/com/wonolodroid/wolibrary/utils/KotExtension.kt
+++ wolibrary/src/main/java/com/wonolodroid/wolibrary/utils/KotExtension.kt
@@ -13,19 +13,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
-import androidx.annotation.AnimRes
-import androidx.annotation.AnimatorRes
-import androidx.annotation.IdRes
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.wonolodroid.R
-import com.wonolodroid.base.CustomTypefaceSpan
 
 fun String?.isNotNullOrEmpty(): Boolean = !this.isNullOrBlank() && this.trim() != "null" && this.trim() != "NULL"
 
@@ -54,75 +46,6 @@ fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>, isUnder
     this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
 
-fun TextView.makeBold(vararg items: Pair<String, Int>, firstIndex: Boolean = true, withoutBoldFirstEndChar: Boolean = false) {
-    val spannableString = SpannableString(this.text)
-    for (item in items) {
-        val startIndexOfLink = if (firstIndex) {
-            this.text.toString().indexOf(item.first)
-        } else {
-            this.text.toString().lastIndexOf(item.first)
-        }
-        if (startIndexOfLink != -1) {
-            val realStartIndex = if (withoutBoldFirstEndChar) {
-                startIndexOfLink + 1
-            } else {
-                startIndexOfLink
-            }
-
-            val endIndex = if (withoutBoldFirstEndChar) {
-                startIndexOfLink + item.first.length - 1
-            } else {
-                startIndexOfLink + item.first.length
-            }
-
-            ResourcesCompat.getFont(context, item.second)?.let {
-                val typefaceSpan = CustomTypefaceSpan("", it)
-                spannableString.setSpan(typefaceSpan, realStartIndex, endIndex, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-            }
-        }
-    }
-    this.setText(spannableString, TextView.BufferType.SPANNABLE)
-}
-
-@JvmOverloads fun FragmentActivity.getCurrentActiveFragment(@IdRes frameId: Int = R.id.content_frame): Fragment? {
-    return supportFragmentManager.findFragmentById(frameId)
-}
-
-@JvmOverloads fun FragmentActivity.addFragment(fragment: Fragment,
-                                 frameId: Int = R.id.content_frame,
-                                 tagName: String? = fragment.javaClass.simpleName,
-                                 useAnimation: Boolean = false,
-                                 addBackStack: Boolean = false,
-                                 @AnimatorRes @AnimRes enter: Int = R.anim.wo_slide_in_right,
-                                 @AnimatorRes @AnimRes exit: Int = R.anim.wo_slide_out_left) {
-    supportFragmentManager.commit(true, body = {
-        if (useAnimation) {
-            setCustomAnimations(enter, exit)
-        }
-        if (addBackStack) {
-            addToBackStack(tagName)
-        }
-        add(frameId, fragment, tagName)
-    })
-}
-
-@JvmOverloads fun FragmentActivity.replaceFragment(fragment: Fragment,
-                                     frameId: Int = R.id.content_frame,
-                                     tagName: String? = fragment.javaClass.simpleName,
-                                     useAnimation: Boolean = false,
-                                     addBackStack: Boolean = false,
-                                     @AnimatorRes @AnimRes enter: Int = R.anim.wo_slide_in_right,
-                                     @AnimatorRes @AnimRes exit: Int = R.anim.wo_slide_out_left) {
-    supportFragmentManager.commit(true) {
-        if (useAnimation) {
-            setCustomAnimations(enter, exit)
-        }
-        if (addBackStack) {
-            addToBackStack(tagName)
-        }
-        replace(frameId, fragment, tagName)
-    }
-}
 
 fun Activity?.isAlive(): Boolean {
     return this?.isFinishing == false && !this.isDestroyed
